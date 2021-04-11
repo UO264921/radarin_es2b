7f140b7171e706aed755f66bea2e38198f83254a
Iyán Sanjurjo <71704577+UO264921@users.noreply.github.com>
2021-04-11 15:25:01 +0200

    Implementada la edición del perfil

--- webapp/src/ui/profile/Profile.js
+++ webapp/src/ui/profile/Profile.js
@@ -1,4 +1,4 @@
-// External dependences
+import { useState } from "react";
 import {
   useSession,
   CombinedDataProvider,
@@ -9,19 +9,24 @@ import {
 import {
   Card,
   CardActionArea,
+  CardActions,
   CardContent,
   Container,
   Typography,
+  IconButton,
 } from "@material-ui/core";
+
+import EditIcon from "@material-ui/icons/Edit";
 import { FOAF, VCARD } from "@inrupt/lit-generated-vocab-common";
+import "./Profile.css";
 
-// Dependences from: ~/ui/profile
-import "./profile.css";
-
-const Profile = () => {
+const Perfil = () => {
   const { session } = useSession();
   const { webId } = session.info;
-
+  const [editing, setEditing] = useState(false);
+  const [textEdit, setText] = useState("Editar perfil");
+  const [colorEdit, setColor] = useState("#E5DBD4");
+ 
   return (
     <Container className="fixed">
       <CombinedDataProvider datasetUrl={webId} thingUrl={webId}>
@@ -30,7 +35,13 @@ const Profile = () => {
             <span className="miPerfil">Mi Perfil</span>
           </Typography>
           <CardActionArea>
-            <Image className="perfil" property={VCARD.hasPhoto.iri.value} />
+            <Image
+              className="perfil"
+              property={VCARD.hasPhoto.iri.value}
+              edit={editing}
+              autosave
+            />
+            <br />
           </CardActionArea>
           <hr />
           <CardContent>
@@ -39,21 +50,67 @@ const Profile = () => {
             </Typography>
             <hr className="line" />
             <Typography>
-              <span className="perfil-span">Nombre:</span><br /> <Text className="text" property={FOAF.name.iri.value} />
+              <span className="perfil-span">Nombre del pod:</span>
+              <br />{" "}
+              <Text
+                className="text"
+                property={FOAF.name.iri.value}
+                edit={editing}
+                autosave
+              />
             </Typography>
             <hr className="line" />
             <Typography>
-              <span className="perfil-span">Descripcion:</span> <br /><Text className="text" property={VCARD.note.iri.value} />
+              <span className="perfil-span">Nombre completo:</span>
+              <br />{" "}
+              <Text
+                className="text"
+                property={VCARD.fn.iri.value}
+                edit={editing}
+                autosave
+              />
+            </Typography>
+            <hr className="line" />
+            <Typography>
+              <span className="perfil-span">Descripcion:</span> <br />
+              <Text
+                className="text"
+                property={VCARD.note.iri.value}
+                edit={editing}
+                autosave
+              />
+              <hr className="line" />
             </Typography>
           </CardContent>
-          <hr />
+          <CardActions>
+            <IconButton
+              class="edit"
+              size="small"
+              style={{backgroundColor : colorEdit }}
+              onClick={() => {
+                setEditing(!editing);
+                textEdit === "Confirmar"
+                  ? setText("Editar perfil")
+                  : setText("Confirmar");
+                colorEdit === "#A4BC96"
+                  ? setColor("#E5DBD4")
+                  : setColor("#A4BC96");
+              }}
+            >
+              {textEdit} <EditIcon />
+            </IconButton>
+          </CardActions>
+          <br />
           <LogoutButton>
-            <button className="botonLogout"><span className="logout">Logout</span></button>
+            <button className="botonLogout">
+              <span className="logout">Logout</span>
+            </button>
           </LogoutButton>
+          <hr />
         </Card>
       </CombinedDataProvider>
     </Container>
   );
 };
 
-export default Profile;
+export default Perfil;
