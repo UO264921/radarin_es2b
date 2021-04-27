eb53c3c6f2063a0582922ac2ec98c5b05f3b04a1
uo269736 <uo269736@uniovi.es>
2021-04-27 16:44:33 +0200

    Encriptando contraseña del administrador

--- webapp/src/ui/admin/RAdmin.js
+++ webapp/src/ui/admin/RAdmin.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import CryptoJS from "crypto-js"
 
 class RAdmin extends React.Component {
 
   async acceder(password){
-    if(password==="hola"){
+    var hash = CryptoJS.SHA256(password);
+    hash=hash.toString(CryptoJS.enc.Base64)
+    console.log(hash)
+    if(hash==="siHZ27CDp/M0KNfCo8MZiuklYU1wIQ4ocWzKp81N23k="){
       let url=window.location.toString()
       url=url.replace("radmin","administrar")
       window.location.href =url;
