55af8bcbdcbb4c7818d4bf423d8e3e5490ac1223
Andres <asdfandres09@gmail.com>
2021-04-30 00:27:03 +0200

    Cambiamos base de datos

--- restapi/server.js
+++ restapi/server.js
@@ -4,16 +4,9 @@ const cors = require('cors');
 const mongoose = require("mongoose")
 const api = require("./api") 
 
-const USER = "admin";
-const PASSWORD = "ADMSIS123";
-const DATA_BASE = "radarin-es2b";
-
-// Preparando cadena de conexion
-const CONECTOR = `mongodb://${USER}:${PASSWORD}@radarin-es2b-shard-00-00.oh0ak.mongodb.net:27017,radarin-es2b-shard-00-01.oh0ak.mongodb.net:27017,radarin-es2b-shard-00-02.oh0ak.mongodb.net:27017/${DATA_BASE}?ssl=true&replicaSet=atlas-6o8ba3-shard-0&authSource=admin&retryWrites=true&w=majority`;
-
 function connect(){
     //The MONGO_URI variable is the connection string to MongoDB Atlas (for production). This env variable is created in heroku.
-    mongo_uri = process.env.MONGO_URI || CONECTOR
+    mongo_uri = process.env.MONGO_URI 
     mongoose.connect(mongo_uri, { useNewUrlParser: true,useUnifiedTopology: true,useFindAndModify: false, useCreateIndex:true}).then(() => {
         const app = express()
 
