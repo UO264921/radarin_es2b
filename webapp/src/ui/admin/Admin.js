49b2765b3db7a89bd2db4817d46c4ad3ef2fa33e
Andres <asdfandres09@gmail.com>
2021-04-30 04:20:05 +0200

    Cambio de estructura y realizacion de tests admin.js

--- webapp/src/ui/admin/Admin.js
+++ webapp/src/ui/admin/Admin.js
@@ -1,43 +1,66 @@
 import React from 'react';
-import {getUsuarios, bloquearUsuario, desbloquearUsuario} from "../../api/api"
+import ReactDOM from 'react-dom';
+import {Button} from "@material-ui/core";
+import {desbloquearUsuario,bloquearUsuario, getUsuarios} from "../../api/api.js"
 
-class Admin extends React.Component {
+let usuarios=[]
 
-  async listarUsuarios() {
-    var list = ""
-    var usuarios = await getUsuarios();
-    usuarios.forEach((usuario) => {
-      list += '<div className="card" ><div><h4 className="peticiones"><p>Usuario: ' + usuario.nombreUsuario + ' <br> WebId: ' +usuario.webid + '</p></h4>' +
-        '<center><div className="botones">' +
-        '<Button variant="contained" className="buttoncard" name="Desbloquear" id='+usuario.webid+' datatype="button" >Desbloquear</Button>'+
-        '<Button variant="contained" className="buttoncard" name="Bloquear" id='+usuario.webid+' datatype="button" >Bloquear</Button>'+
-        '</div></center></div></div>' ;
-    })
-    var lista = document.getElementById("usuarios");
-    lista.innerHTML = list;
-    if (lista !== "") {
-      let elementA = document.getElementsByName('Desbloquear');
-      elementA.forEach((element) => element.onclick = ()=> desbloquearUsuario(element.id));
-      let elementB = document.getElementsByName('Bloquear');
-      elementB.forEach((element) => element.onclick = ()=> bloquearUsuario(element.id));
-    }  
-  }
-  
-  render() {
-    return (
-      <div title="Admin">
-        <div className="prueba">
-          <h2>Administrar Usuarios</h2>
-          <button onClick={()=>this.listarUsuarios()}>Mostrar Usuarios</button>
-          <div id="usuarios" className="list" padding-inline-start="0">
-          </div>
-        </div>
-        <button ><a href="/login" >Volver</a></button>
-      </div>
-    )
-  }
+export async function ListarUsuarios() {
+  usuarios= await getUsuarios()
+    ReactDOM.render(
+      <ListaUsuarios/>
+         ,
+      document.getElementById('usuarios')
+    );
 }
 
+export const ListaUsuarios = ({usauriosOpcional})=>{
+  if(usauriosOpcional)
+    usuarios=usauriosOpcional
+  var listItems = usuarios.map( (usuario) => 
+    <li key="{item}"><Usuario usuario={usuario} desbloquear={()=>desbloquearUsuario(usuario.webid)} bloquear={()=>bloquearUsuario(usuario.webid)}/></li>
+  );
+    return(
+      <ul className="user-list">
+        {listItems}
+      </ul>
+    )
+}
 
+export const Usuario =({usuario, desbloquear, bloquear})=>{
+  var nombre=usuario.nombreUsuario;
+  var webid=usuario.webid;
+  const desblo = desbloquear
+  const blo = bloquear
+    return(
+        <div className="card" >
+            <div>
+                <h4 className="lista">
+                    <p>Usuario:</p><span>{nombre}</span><br/><p>WebId:</p><span>{webid}</span>
+                </h4> 
+                <center>
+                    <div className="botones"> 
+                        <Button variant="contained" className="buttoncard" name="Desbloquear" id='usuario.webid' datatype="button" onClick={desblo}>Desbloquear</Button>
+                        <Button variant="contained" className="buttoncard" name="Bloquear" id='usuario.webid' datatype="button" onClick={blo }>Bloquear</Button>
+                    </div>
+                </center>
+            </div>
+        </div>
+    );
+}
+
+export const Admin = ({method}) => {
+  return (
+    <div title="Admin">
+      <div className="prueba">
+        <h2>Administrar Usuarios</h2>
+        <button onClick={method}>Mostrar Usuarios</button>
+        <div id="usuarios" className="list" padding-inline-start="0">
+        </div>
+      </div>
+      <button ><a href="/login" >Volver</a></button>
+    </div>
+  )
+}
 
 export default Admin;
