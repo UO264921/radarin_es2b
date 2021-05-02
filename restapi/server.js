aa8cf24fcef2c06be4773a78a6e03af5bf0291f1
Iyán Sanjurjo <71704577+UO264921@users.noreply.github.com>
2021-05-02 18:15:21 +0200

    Posible solucion base de datos

--- restapi/server.js
+++ restapi/server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const promBundle = require("express-prom-bundle");
-//const cors = require('cors');
+const cors = require('cors');
 const mongoose = require("mongoose")
 const api = require("./api")
 
@@ -20,17 +20,18 @@ function connect() {
         const metricsMiddleware = promBundle({ includeMethod: true });
         app.use(metricsMiddleware);
 
-        //app.use(cors());
-        //app.options('*', cors());
+        app.use(cors());
+        app.options('*', cors());
         app.use(express.json())
         app.use("/api", api)
 
         app.use( (request, response) => {
-            request.header("Access-Control-Allow-Origin", "https://radarines2bwebapp.herokuapp.com/*");
-            request.header("Access-Control-Allow-Origin", "https://radarines2brestapi.herokuapp.com/*");
-            request.header("Access-Control-Allow-Origin", "http://localhost:3000/*"); // just for working until the final delivery and all deploy
-            request.header("Access-Control-Allow-Origin", "http://localhost:5000/api/*"); // " "
-            request.header("GET, POST"); // the other methods are not used in the api
+            request.header("Access-Control-Allow-Origin", "*");
+            //request.header("Access-Control-Allow-Origin", "https://radarines2bwebapp.herokuapp.com/*");
+            //request.header("Access-Control-Allow-Origin", "https://radarines2brestapi.herokuapp.com/*");
+            //request.header("Access-Control-Allow-Origin", "http://localhost:3000/*"); // just for working until the final delivery and all deploy
+            //request.header("Access-Control-Allow-Origin", "http://localhost:5000/api/*"); // " "
+            request.header("'Access-Control-Allow-Methods: GET, POST"); // the other methods are not used in the api
         });
 
 
