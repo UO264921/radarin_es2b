dca25e03fdb8e6d5d5a67f19a9b7e3c4999fc672
Andres <asdfandres09@gmail.com>
2021-05-02 02:20:37 +0200

    Arreglo de CI

--- webapp/src/persistence/user/UserGateway.test.js
+++ webapp/src/persistence/user/UserGateway.test.js
@@ -3,5 +3,5 @@ import UserGateway from "./UserGateway";
 test('Comprobar que la factoria funciona correctamente', async () => {
    var userGateway=new UserGateway();
    expect(await userGateway.getUsername("webid")).toStrictEqual(undefined);
-   expect(await userGateway.updateCoords("webid","30,30")).toStrictEqual(true);
+   expect(await userGateway.updateCoords("webid","30,30")).toStrictEqual(undefined);
 });
